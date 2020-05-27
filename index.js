532e179bf0daf5531e2565a34fe55b34918f2815
HongSenDu <hongsendu@gmail.com>
2020-05-26 20:20:32 -0400

    updated index

--- index.js
+++ index.js
@@ -4,28 +4,15 @@ const START_SEARCH_NO = 'START_SEARCH_NO';
 const START_SEARCH_YES = 'START_SEARCH_YES';
 const GREETING = 'GREETING';
 const AUSTRALIA_YES = 'AUSTRALIA_YES';
-const AU_LOC_PROVIDED = 'AU_LOC_PROVIDED';
-const PREFERENCE_PROVIDED = 'PREFERENCE_PROVIDED';
-const PREF_CLEANUP = 'PREF_CLEANUP';
-const PREF_REVEGETATION = 'PREF_REVEGETATION';
-const PREF_BIO_SURVEY = 'PREF_BIO_SURVEY';
-const PREF_CANVASSING = 'PREF_CANVASSING';
 const AUSTRALIA_NO = 'AUSTRALIA_NO';
 const OTHER_HELP_YES = 'OTHER_HELP_YES';
 const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
-const GOOGLE_GEOCODING_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-const MONGODB_URI = process.env.MONGODB_URI;
-const GOOGLE_GEOCODING_API_KEY = process.env.GOOGLE_GEOCODING_API_KEY;
 
 const
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
-  mongoose = require('mongoose'),
   app = express().use(body_parser.json()); // creates express http server
-
- var db = mongoose.connect(MONGODB_URI);
- var ChatStatus = require("./models/chatstatus");
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -33,38 +20,42 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
 
-  // Return a '200 OK' response to all events
-  res.status(200).send('EVENT_RECEIVED');
+  // Parse the request body from the POST
+  let body = req.body;
 
-  const body = req.body;
-
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-      // Iterate over each entry
-      // There may be multiple if batched
-      if (body.entry && body.entry.length <= 0){
-        return;
+
+    body.entry.forEach(function(entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log('Webhook event:', webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender ID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      } else if (webhook_event.message) {
+        console.log('webhook_event.message', JSON.stringify(webhook_event.message));
+        console.log('webhook_event.message["quick_reply"]', webhook_event.message["quick_reply"]);
+        if (webhook_event.message.quick_reply){
+          handlePostback(sender_psid, webhook_event.message.quick_reply);
+        } else{
+          handleMessage(sender_psid);
+        }
       }
-      body.entry.forEach((pageEntry) => {
-        // Iterate over each messaging event and handle accordingly
-        pageEntry.messaging.forEach((messagingEvent) => {
-          console.log({messagingEvent});
-          if (messagingEvent.postback) {
-            handlePostback(messagingEvent.sender.id, messagingEvent.postback);
-          } else if (messagingEvent.message) {
-            if (messagingEvent.message.quick_reply){
-              handlePostback(messagingEvent.sender.id, messagingEvent.message.quick_reply);
-            } else{
-              handleMessage(messagingEvent.sender.id, messagingEvent.message);
-            }
-          } else {
-            console.log(
-              'Webhook received unknown messagingEvent: ',
-              messagingEvent
-            );
-          }
-        });
-      });
-    }
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 });
 
 // Accepts GET requests at the /webhook endpoint
@@ -95,145 +86,23 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-function handleMessage(sender_psid, message) {
-  // check if it is a location message
-  console.log('handleMEssage message:', JSON.stringify(message));
-
-  const locationAttachment = message && message.attachments && message.attachments.find(a => a.type === 'location');
-  const coordinates = locationAttachment && locationAttachment.payload && locationAttachment.payload.coordinates;
-
-  if (coordinates && !isNaN(coordinates.lat) && !isNaN(coordinates.long)){
-    handleMessageWithLocationCoordinates(sender_psid, coordinates.lat, coordinates.long);
-    return;
-  } else if (message.nlp && message.nlp.entities && message.nlp.entities.location && message.nlp.entities.location.find(g => g.confidence > 0.8 && g.suggested)){
-    const locationName = message.nlp.entities.location.find(loc => loc.confidence > 0.8 && loc.suggested);
-    if (locationName.value){
-      const locationNameEncoded = encodeURIComponent(locationName.value);
-      callGeocodingApi(locationNameEncoded, sender_psid, handleConfirmLocation);
-    }
-    return;
-  } else if (message.nlp && message.nlp.entities && message.nlp.entities.greetings && message.nlp.entities.greetings.find(g => g.confidence > 0.8 && g.value === 'true')){
-    handlePostback(sender_psid, {payload: GREETING});
-    return;
-  }
-}
-
-function handleConfirmLocation(sender_psid, geocoding_location, geocoding_formattedAddr){
-  console.log('Geocoding api result: ', geocoding_location);
-  const query = {$and: [{'user_id': sender_psid}, { 'status': AUSTRALIA_YES }]};
-  const update = {
-    $set: { "location.lat": geocoding_location.lat, "location.long": geocoding_location.lng, status: AU_LOC_PROVIDED }
-  };
-  const options = {upsert: false, new: true};
-
-  ChatStatus.findOneAndUpdate(query, update, options, (err, cs) => {
-    console.log('handleConfirmLocation update location:', cs);
-    if (err){
-      console.log('handleConfirmLocation Error in updating coordinates:', err);
-    } else if (cs){
-      const response = {
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text":`${geocoding_formattedAddr}. Is it your address?`,
-            "buttons":[
-              {
-                "type":"postback",
-                "payload": AU_LOC_PROVIDED,
-                "title":"Yes"
-              },
-              {
-                "type":"postback",
-                "payload": AUSTRALIA_YES,
-                "title":"No"
-              }
-            ]
-          }
-        }
-      };
-      callSendAPI(sender_psid, response);
-    }
-  });
-}
-
-function handleMessageWithLocationCoordinates(sender_psid, coordinates_lat, coordinates_long){
-  const query = {$and: [
-    { 'user_id': sender_psid },
-    { 'status': AUSTRALIA_YES }
-  ]};
-  const update = {
-    $set: { "location.lat": coordinates_lat, "location.long": coordinates_long, status: AU_LOC_PROVIDED }
-  };
-  const options = {upsert: false, new: true};
-
-  ChatStatus.findOneAndUpdate(query, update, options, (err, cs) => {
-    console.log('handleMessage update coordinates:', cs);
-    if (err){
-      console.log('Error in updating coordinates:', err);
-    } else if (cs){
-      askForActivityPreference(sender_psid);
-    }
-  });
-}
-
-function askForActivityPreference(sender_psid){
+function handleMessage(sender_psid) {
   const response = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "list",
-        "top_element_style": "compact",
-        "elements": [
-          {
-            "title": "Environmental Cleanup",
-            "subtitle": "Clean environment",
-            "image_url": "http://www.wwf.org.au/Images/UserUploadedImages/416/img-bait-reef-coral-bleaching-rubble-1000px.jpg",
-            "buttons": [
-              {
-                type: "postback",
-                title: "Go Environmental Cleanup",
-                payload: PREF_CLEANUP
-              }
-            ]
-          }, {
-            "title": "Revegetation",
-            "subtitle": "Revegetation",
-            "image_url": "http://www.wwf.org.au//Images/UserUploadedImages/416/img-planet-globe-on-moss-forest-1000px.jpg",
-            "buttons": [
-              {
-                type: "postback",
-                title: "Go Revegetation",
-                payload: PREF_REVEGETATION
-              }
-            ]
-          }, {
-            "title": "Bio Survey",
-            "subtitle": "Bio Survey",
-            "image_url": "http://www.wwf.org.au/Images/UserUploadedImages/416/img-koala-in-tree-1000px.jpg",
-            "buttons": [
-              {
-                type: "postback",
-                title: "Go Bio Survey",
-                payload: PREF_BIO_SURVEY
-              }
-            ]
-          }, {
-            "title": "Canvassing",
-            "subtitle": "Canvassing",
-            "image_url": "http://www.wwf.org.au/Images/UserUploadedImages/416/img-hackathon-winners-2017-1000px.jpg",
-            "buttons": [
-              {
-                type: "postback",
-                title: "Go Canvassing",
-                payload: PREF_CANVASSING
-              }
-            ]
-          }
-        ]
+    "text": "Hi, it would take me some times to answer your message. Are you looking for opportunities to join a community of like-minded pandas in your area?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Yes!",
+        "payload": START_SEARCH_YES
+      },{
+        "content_type":"text",
+        "title":"No, thanks.",
+        "payload": START_SEARCH_NO
       }
-    }
+    ]
   };
+
+  // Send the response message
   callSendAPI(sender_psid, response);
 }
 
@@ -346,122 +215,37 @@ function handleAustraliaYesPostback(sender_psid){
     "text": "Where about do you live?",
     "quick_replies":[
       {
-        "content_type":"location"
+        "content_type":"location",
+        "payload": "TEST_LOCATION"
       }
     ]
   };
   callSendAPI(sender_psid, askForLocationPayload);
 }
 
-function handlePreferencePostback(sender_psid, chatStatus){
-  console.log('handlePreferencePostback params: ', chatStatus);
-  if (chatStatus && !isNaN(chatStatus.location.lat) && !isNaN(chatStatus.location.long)){
-    request({
-      "url": `${FACEBOOK_GRAPH_API_BASE_URL}search?type=page&q=NonProfit+Australia&fields=name,id,category,location,picture`,
-      "qs": { "access_token": PAGE_ACCESS_TOKEN },
-      "method": "GET"
-    }, (err, res, body) => {
-      if (err) {
-        console.error("Unable to search Facebook API:" + err);
-      } else {
-          console.log("Facebook API result:", body);
-          let bodyJson = JSON.parse(body);
-          let elements = bodyJson.data.filter(d => {
-            if (isNaN(d.location && d.location.latitude) || isNaN(d.location && d.location.longitude)){
-              return false;
-            }
-            return d.location.latitude < chatStatus.location.lat + 0.1 && d.location.latitude > chatStatus.location.lat - 0.1
-              && d.location.longitude < chatStatus.location.long + 0.1 && d.location.longitude > chatStatus.location.long - 0.1
-          }).slice(0,3).map(org => {
-              let element = {
-                "title": org.name,
-                "buttons":[{
-                  "type": "web_url",
-                  "url": `https://www.facebook.com/${org.id}`,
-                  "title": org.name,
-                }]
-              };
-              if (org.category){
-                element["subtitle"] = org.category;
-              }
-
-              if (org.picture && org.picture.data && org.picture.data.url){
-                element["image_url"] = org.picture.data.url;
-              }
-              console.log("Facebook API element:", element);
-              return element;
-          });
-          console.log("Facebook API elements:", elements);
-          const organizationPayload = {
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
-                "elements": elements
-              }
-            }
-          };
-          callSendAPI(sender_psid, organizationPayload);
-        }
-    });
-  }
-}
-
-function updateStatus(sender_psid, status, callback){
-  const query = {user_id: sender_psid};
-  const update = {status: status};
-  const options = {upsert: status === GREETING};
-
-  ChatStatus.findOneAndUpdate(query, update, options).exec((err, cs) => {
-    console.log('update status to db: ', cs);
-    callback(sender_psid);
-  });
-}
-
-function updatePreference(sender_psid, perference, callback){
-  const query = {user_id: sender_psid};
-  const update = {status: 'PREFERENCE_PROVIDED', preference: perference};
-  const options = {upsert: false, new: true};
-
-  ChatStatus.findOneAndUpdate(query, update, options).exec((err, cs) => {
-    console.log('update perference to db: ', cs);
-    callback(sender_psid, cs);
-  });
-}
-
 function handlePostback(sender_psid, received_postback) {
   // Get the payload for the postback
   const payload = received_postback.payload;
-
-  // Set the response and udpate db based on the postback payload
+  // Set the response based on the postback payload
   switch (payload){
     case START_SEARCH_YES:
-      updateStatus(sender_psid, payload, handleStartSearchYesPostback);
+      handleStartSearchYesPostback(sender_psid);
       break;
     case START_SEARCH_NO:
-      updateStatus(sender_psid, payload, handleStartSearchNoPostback);
+      handleStartSearchNoPostback(sender_psid);
       break;
     case OTHER_HELP_YES:
-      updateStatus(sender_psid, payload, handleOtherHelpPostback);
+      handleOtherHelpPostback(sender_psid);
       break;
     case AUSTRALIA_YES:
-      updateStatus(sender_psid, payload, handleAustraliaYesPostback);
-      break;
-    case AU_LOC_PROVIDED:
-      updateStatus(sender_psid, payload, askForActivityPreference);
+      handleAustraliaYesPostback(sender_psid);
       break;
     case GREETING:
-      updateStatus(sender_psid, payload, handleGreetingPostback);
-      break;
-    case PREF_CLEANUP:
-    case PREF_REVEGETATION:
-    case PREF_BIO_SURVEY:
-    case PREF_CANVASSING:
-      updatePreference(sender_psid, payload, handlePreferencePostback);
+      handleGreetingPostback(sender_psid);
       break;
     default:
-      console.log('Cannot differentiate the payload type');
+      console.log('Cannot differentiate the payload type, treat it as a emtpy message');
+      handleMessage(sender_psid);
   }
 }
 
@@ -477,36 +261,14 @@ function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "url": `${FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
+    "uri": `${FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
-    console.log("Message Sent Response body:", body);
     if (err) {
-      console.error("Unable to send message:", err);
+      console.error("Unable to send message:" + err);
     }
   });
 }
 
-function callGeocodingApi(address, sender_psid, callback){
-  console.log('before calling geocoding api with address:', address);
-  request({
-    "url": `${GOOGLE_GEOCODING_API}${address}&key=${GOOGLE_GEOCODING_API_KEY}`,
-    "method": "GET"
-  }, (err, res, body) => {
-    console.log('after calling geocoding api with result:', body);
-    if (err) {
-      console.error("Unable to retrieve location from Google API:", err);
-    } else {
-      const bodyObj = JSON.parse(body);
-      if (bodyObj.status === 'OK'){
-        if (bodyObj.results && bodyObj.results[0] && bodyObj.results[0].geometry && bodyObj.results[0].geometry.location){
-          callback(sender_psid, bodyObj.results[0].geometry.location, bodyObj.results[0].formatted_address);
-        }
-      } else{
-        console.error("Unable to retrieve location (status non-OK):", bodyObj);
-      }
-    }
-  });
-}
