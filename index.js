b86d9e09f713ebb910ce7c20ba69bb6991a032cf
HongSenDu <hongsendu@gmail.com>
2020-05-26 23:16:59 -0400

    phone number dependency

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ function handlePostback(sender_psid, received_postback) {
     case payload === AUSTRALIA_YES:
       handleAustraliaYesPostback(sender_psid);
       break;
-    case (parsePhoneNumberFromString(payload).isValid()):
+    case phone.parsePhoneNumberFromString(payload).isValid():
       handleTestPhone(sender_psid);
       break;
     case payload === GREETING:
