174620684851e86c2104acc03ec797e9c14222cb
HongSenDu <hongsendu@gmail.com>
2020-05-26 23:51:56 -0400

    why am i so bad

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ function handlePostback(sender_psid, received_postback) {
     case payload === AUSTRALIA_YES:
       handleAustraliaYesPostback(sender_psid);
       break;
-    case phoneUtil.isValidNumber(phoneUtil.parse(payload)):
+    case phoneUtil.isValidNumber(phoneUtil.parse(payload)) === true:
       handleTestPhone(sender_psid);
       break;
     case payload === GREETING:
